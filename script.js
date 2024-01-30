919868a1ba77fc8e011e4f4eaf0d44b9462397ac
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-01-29 21:00:10 -0500

    added task cross out and uncross out JS

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ function addTask() {
     let li = document.createElement("li"); //creates an HTML element via li
     li.innerHTML = inputBox.value; // the text the users adds
     listContainer.appendChild(li); // displays li text in the listcontainer div
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; // Adds cross 'x' icon
+    li.appendChild(span);
   }
+  inputBox.value = "";
 }
+
+//  For click crosses out or uncrosses out task & changes check png 
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+
